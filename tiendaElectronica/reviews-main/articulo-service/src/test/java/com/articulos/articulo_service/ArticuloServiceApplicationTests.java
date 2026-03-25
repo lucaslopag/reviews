d14fbaed90f9be4d7e216e09427a0b0b07eae20b
lucaslopag <96917092+lucaslopag@users.noreply.github.com>
2026-03-25 12:00:01 +0100

    Add files via upload

---
+++ tiendaElectronica/reviews-main/articulo-service/src/test/java/com/articulos/articulo_service/ArticuloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.articulos.articulo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticuloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
